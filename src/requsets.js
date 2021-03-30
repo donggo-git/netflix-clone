76b1048cf6b62fb7d994547b6ce3272cbd6791ce
Dong <Dlttthvlhhth@gmail.com>
2021-03-30 15:18:01 -0700

    create request

---
+++ src/requsets.js
@@ -0,0 +1,13 @@
+const API_KEY = "d03ccd3f8ae255e9b5fa0f7c48581e8c"
+
+const request = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key${API_KEY}&with_genres=27`,
+    fetchRomanMovies: `/discover/movie?api_key${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key${API_KEY}&with_genres=99`
+}
+export default request;
