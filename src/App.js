693f7dd4051162c6f873233e8a274f98d32ccd7a
Dong <Dlttthvlhhth@gmail.com>
2021-03-30 23:15:43 -0700

    create row fix fetch for request from action movie

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import './App.css';
 import Row from './Row'
+import requests from './requsets'
 
 function App() {
   return (
     <div className="App">
-      <Row title />
+      <Row title='NETFLIX ORIGINAL' fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+
+      <Row title='Trending Now' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Trending Now' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Trending Now' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Trending Now' fetchUrl={requests.fetchRomanMovies} />
+      <Row title='Trending Now' fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
